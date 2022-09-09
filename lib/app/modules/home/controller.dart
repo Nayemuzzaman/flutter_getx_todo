d3da0330df01fe0518c5749a6e8ad20da68c2ca8
nayemuzzaman <nayemzaman@gmail.com>
2022-09-10 03:19:31 +0600

    finish task

--- lib/app/modules/home/controller.dart
+++ lib/app/modules/home/controller.dart
@@ -10,6 +10,7 @@ class HomeController extends GetxController {
   HomeController({required this.taskRepository});
   final fromKey = GlobalKey<FormState>();
   final editCtrl = TextEditingController();
+  final tabIndex = 0.obs;
   final chipIndex = 0.obs;
   final deleting = false.obs;
   final tasks = <Task>[].obs;
@@ -28,6 +29,10 @@ class HomeController extends GetxController {
   void onClose() {
     editCtrl.dispose();
     super.onClose();
+  }
+
+  void changeTabIndex(int index) {
+    tabIndex.value = index;
   }
 
   void changeChipIndex(int value) {
@@ -140,6 +145,30 @@ class HomeController extends GetxController {
     for (int i = 0; i < task.todos!.length; i++) {
       if (task.todos![i]['done'] == true) {
         res += 1;
+      }
+    }
+    return res;
+  }
+
+  int getTotalTask() {
+    var res = 0;
+    for (int i = 0; i < tasks.length; i++) {
+      if (tasks[i].todos != null) {
+        res += tasks[i].todos!.length;
+      }
+    }
+    return res;
+  }
+
+  int getTotalDoneTask() {
+    var res = 0;
+    for (int i = 0; i < tasks.length; i++) {
+      if (tasks[i].todos != null) {
+        for (int j = 0; j < tasks[i].todos!.length; j++) {
+          if (tasks[i].todos![j]['done'] == true) {
+            res += 1;
+          }
+        }
       }
     }
     return res;
